07eb0649bbc30cabb0d644ce89e63632fddd0f50
James Thompson <jamesrth@usc.edu>
2013-06-13 13:50:39 -0700

    First commit
    Countdown word game solver Scalaz

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "Functional Countdown"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies ++= Seq(
+  "org.scalaz" %% "scalaz-core" % "7.0.0",
+  "org.scalaz" %% "scalaz-effect" % "7.0.0",
+  "org.scalaz" %% "scalaz-typelevel" % "7.0.0"
+)
+
+mainClass in (Compile, run) := Some("cntdn.Run")
+
+
+
+
---
+++ src/main/scala/Run.scala
@@ -0,0 +1,27 @@
+package cntdn
+
+import io.Source
+import scalaz._, Scalaz._, effect._
+
+object Run extends App {
+	
+	// Create the unsafe IO operation that will change load the words
+	val loadDictionaryAction = IO { 
+		Source.fromFile("/Users/James/Desktop/cntdn/src/main/resources/dictionary.txt").getLines.toStream 
+	}
+
+	// Change the world by loading the dictionary!
+	val words = loadDictionaryAction.unsafePerformIO.toList
+
+	// Characters we can use - first half of the alphabet for a test!
+	val testString = ('a' |-> 'm') mkString
+
+	// Can a word s be made using the testString characters tstr?
+	def canBeMade(s: List[Char], tstr: String = testString) : Boolean = s match {
+		case Nil 		=> true
+		case h :: t => tstr.contains(h) ? canBeMade(t, tstr diff h.point[List]) | false
+	}
+
+	// Make a list of words than can be made with these characters
+	val canMake = words filter { word => canBeMade(word.toList) } println
+}
