6c7f4630caf53e68a18d2e5db137664e62588143
James Thompson <jamesrth@usc.edu>
2013-06-13 16:47:40 -0700

    Main problem formally specified

--- src/main/scala/NumbersGame.scala
+++ src/main/scala/NumbersGame.scala
@@ -10,9 +10,24 @@ object NumbersGame extends App {
 	case object Multiply extends Op
 	case object Divide extends Op
 
-	case class Expr(v: Int, op: Op, expr1: Expr, expr2: Expr)
+	sealed trait Expr
+	case class Value(val n: Int) extends Expr 
+	case class App(val op: Op, val l: Expr, val r: Expr) extends Expr 
 
-	def calcOp(op: Op, x: Int, y: Int) : Double = op match {
+	def values(expr: Expr) : List[Int] = expr match {
+		case v:Value => List(v.i)
+		case a:App => values(a.l) ++ values(a.r)
+	}
+
+	def eval(expr: Expr) : List[Int] = expr match {
+		case v:Value => (v.n > 0) ? List(v.n) | Nil
+		case a:App => for {
+			x <- eval(a.l),
+			y <- eval(a.r), if(valid(a.op, x, y))
+		} yield List(calcOp(a.op, x, y))
+	}
+
+	def calcOp(op: Op, x: Int, y: Int) : Int = op match {
 		case Add 			=> x + y
 		case Minus 		=> x - y
 		case Multiply => x * y
@@ -25,7 +40,14 @@ object NumbersGame extends App {
 		case Multiply => true
 		case Divide 	=> x % y == 0
 	}
-	
-	
 
+	def elem[T](x: T, ys: List[T]) : Boolean = ys match {
+    case Nil => false
+    case h :: t => x == h || elem(x, t)
+  }
+	
+	def solution(e: Expr, ns: List[Int], n: Int) : Boolean = 
+		elem(values(e), ns.permutations) && calcOp(e) === List(n)
+
+	
 }
