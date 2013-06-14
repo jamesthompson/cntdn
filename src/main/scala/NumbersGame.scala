210731fdc19869768524800ceab07f3393be3f1e
James Thompson <jamesrth@usc.edu>
2013-06-13 17:20:24 -0700

    Updates on logic

--- src/main/scala/NumbersGame.scala
+++ src/main/scala/NumbersGame.scala
@@ -12,7 +12,11 @@ object NumbersGame extends App {
 
 	sealed trait Expr
 	case class Value(val n: Int) extends Expr 
-	case class App(val op: Op, val l: Expr, val r: Expr) extends Expr 
+	case class App(val op: Op, val l: Expr, val r: Expr) extends Expr
+
+	case class Result(expr: Expr, i: Int) {
+		override def toString = ???
+	}
 
 	def values(expr: Expr) : List[Int] = expr match {
 		case v:Value => List(v.i)
@@ -20,7 +24,7 @@ object NumbersGame extends App {
 	}
 
 	def eval(expr: Expr) : List[Int] = expr match {
-		case v:Value => (v.n > 0) ? List(v.n) | Nil
+		case v:Value => (v.n gt 0) ? List(v.n) | Nil
 		case a:App => for {
 			x <- eval(a.l),
 			y <- eval(a.r), if(valid(a.op, x, y))
@@ -35,12 +39,13 @@ object NumbersGame extends App {
 	}
 
 	def valid(op: Op, x: Int, y: Int) : Boolean = op match {
-		case Add 			=> true
-		case Minus 		=> x > y
-		case Multiply => true
-		case Divide 	=> x % y == 0
+		case Add 			=> x lte y
+		case Minus 		=> x gt y
+		case Multiply => x =/= 1 && y =/= 1 && lte y
+		case Divide 	=> y =/= 1 && x % y === 0
 	}
 
+	// Equals here is not typesafe!
 	def elem[T](x: T, ys: List[T]) : Boolean = ys match {
     case Nil => false
     case h :: t => x == h || elem(x, t)
@@ -49,5 +54,18 @@ object NumbersGame extends App {
 	def solution(e: Expr, ns: List[Int], n: Int) : Boolean = 
 		elem(values(e), ns.permutations) && calcOp(e) === List(n)
 
-	
+	def results(values: List[Int]) : List[Result] = values match {
+		case Nil 		=> Nil
+		case List(h) 	=> (h gt 0) ? Result(Value(h), h)
+		case h :: t =>  for {
+			(ls, rs) <- nesplit(values),
+			lx <- results(ls),
+			ry <- results(rs),
+			res <- combine(lx, ry)
+		} yield res
+	}
+
+	//need to implement combine and nesplit
+
+
 }
