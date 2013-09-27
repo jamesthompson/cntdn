2e239be85af58b97815fc2fc379de4d55b391e40
James Thompson <jamesrth@usc.edu>
2013-09-27 11:07:53 -0700

    Fixing tabs

--- src/main/scala/NumbersGame.scala
+++ src/main/scala/NumbersGame.scala
@@ -8,111 +8,109 @@ import scalaz._, Scalaz._, effect._
 
 object NumbersGame extends App {
 
-	// Types
-	
-	sealed trait Op
-	case object Add extends Op { override def toString = "+" }
-	case object Minus extends Op { override def toString = "-" }
-	case object Multiply extends Op { override def toString = "*" }
-	case object Divide extends Op { override def toString = "/" }
+  // Types
+  
+  sealed trait Op
+  case object Add extends Op { override def toString = "+" }
+  case object Minus extends Op { override def toString = "-" }
+  case object Multiply extends Op { override def toString = "*" }
+  case object Divide extends Op { override def toString = "/" }
 
-	sealed trait Expr
-	case class Value(val n: Int) extends Expr { override def toString = n.toString }
-	case class App(val op: Op, val l: Expr, val r: Expr) extends Expr {
-		override def toString = s"(${l.toString} ${op.toString} ${r.toString})"
-	}
+  sealed trait Expr
+  case class Value(val n: Int) extends Expr { override def toString = n.toString }
+  case class App(val op: Op, val l: Expr, val r: Expr) extends Expr {
+    override def toString = s"(${l.toString} ${op.toString} ${r.toString})"
+  }
 
-	case class Result(val expr: Expr, val i: Int) {
-		override def toString = s"${expr.toString} = $i"
-	}
+  case class Result(val expr: Expr, val i: Int) {
+    override def toString = s"${expr.toString} = $i"
+  }
 
-	// Logic
+  // Logic
 
-	def calcOp(op: Op, x: Int, y: Int) : Int = op match {
-		case Add 			=> x + y
-		case Minus 		=> x - y
-		case Multiply => x * y
-		case Divide 	=> x / y
-	}
+  def calcOp(op: Op, x: Int, y: Int) : Int = op match {
+    case Add      => x + y
+    case Minus    => x - y
+    case Multiply => x * y
+    case Divide   => x / y
+  }
 
-	def valid(op: Op, x: Int, y: Int) : Boolean = op match {
-		case Add 			=> x lte y
-		case Minus 		=> x gt y
-		case Multiply => (x =/= 1) && (y =/= 1) && (x lte y)
-		case Divide 	=> (y =/= 1) && (x % y === 0)
-	}
+  def valid(op: Op, x: Int, y: Int) : Boolean = op match {
+    case Add      => x lte y
+    case Minus    => x gt y
+    case Multiply => (x =/= 1) && (y =/= 1) && (x lte y)
+    case Divide   => (y =/= 1) && (x % y === 0)
+  }
 
-	def values(expr: Expr) : List[Int] = expr match {
-		case v:Value => List(v.n)
-		case a:App => values(a.l) ++ values(a.r)
-	}
+  def values(expr: Expr) : List[Int] = expr match {
+    case v:Value => List(v.n)
+    case a:App => values(a.l) ++ values(a.r)
+  }
 
-	def eval(expr: Expr) : List[Int] = expr match {
-		case v:Value => (v.n gt 0) ? List(v.n) | Nil
-		case a:App => for { x <- eval(a.l);
-												y <- eval(a.r); 
-												if(valid(a.op, x, y))
-											} yield calcOp(a.op, x, y)
-	}
+  def eval(expr: Expr) : List[Int] = expr match {
+    case v:Value => (v.n gt 0) ? List(v.n) | Nil
+    case a:App => for { x <- eval(a.l);
+                        y <- eval(a.r); 
+                        if(valid(a.op, x, y))
+                      } yield calcOp(a.op, x, y)
+  }
 
-	def solution(e: Expr, ns: List[Int], n: Int) : Boolean = 
-		elem(values(e), subbags(ns)) && eval(e) === List(n)
+  def solution(e: Expr, ns: List[Int], n: Int) : Boolean = 
+    elem(values(e), subbags(ns)) && eval(e) === List(n)
 
-	def combine(lres: Result, rres: Result) : List[Result] = {
-		val (l, x) = (lres.expr, lres.i) 
-		val (r, y) = (rres.expr, rres.i)
-		for { o <- List(Add, Minus, Multiply, Divide);
-					if(valid(o, x, y))
-				} yield Result(App(o, l, r), calcOp(o, x, y))
-	}
+  def combine(lres: Result, rres: Result) : List[Result] = {
+    val (l, x) = (lres.expr, lres.i) 
+    val (r, y) = (rres.expr, rres.i)
+    for { o <- List(Add, Minus, Multiply, Divide);
+          if(valid(o, x, y))
+        } yield Result(App(o, l, r), calcOp(o, x, y))
+  }
 
-	def results(values: List[Int]) : List[Result] = values match {
-		case Nil 		 => Nil
-		case List(h) => (h gt 0) ? List(Result(Value(h), h)) | Nil
-		case h :: t  => for { (ls, rs) <- nesplit(values);
-													lx <- results(ls);
-													ry <- results(rs);
-													res <- combine(lx, ry).view
-												} yield res
-	}
+  def results(values: List[Int]) : List[Result] = values match {
+    case Nil     => Nil
+    case List(h) => (h gt 0) ? List(Result(Value(h), h)) | List()
+    case h :: t  => for { (ls, rs) <- nesplit(values);
+                          lx <- results(ls);
+                          ry <- results(rs);
+                          res <- combine(lx, ry).view
+                        } yield res
+  }
 
-	def solutions(ns: List[Int], target: Int) : List[Result] = 
-		for { nss <- subbags(ns);
-					r <- results(nss);
-					if(r.i === target)
-		} yield r
+  def solutions(ns: List[Int], target: Int) : List[Result] = 
+    for { nss <- subbags(ns);
+          r <- results(nss);
+          if(r.i === target)
+    } yield r
 
 
-	// Helper methods
+  // Helper functions
 
-	def elem[T](x: T, ys: List[T]) : Boolean = ys match {
-    case Nil 		=> false
+  def elem[T](x: T, ys: List[T]) : Boolean = ys match {
+    case Nil    => false
     case h :: t => x == h || elem(x, t)
   }
 
-	def nesplit[T](l: List[T]) : List[(List[T], List[T])] = 
-		l.indices.toList map { l.splitAt(_) } filterNot { t => (t._1.isEmpty || t._2.isEmpty) }
+  def nesplit[T](l: List[T]) : List[(List[T], List[T])] = 
+    l.indices.toList map { l.splitAt(_) } filterNot { t => (t._1.isEmpty || t._2.isEmpty) }
 
-	def subs[T](l: List[T]) : List[List[T]] = l match {
-		case Nil => List(List())
-		case h :: t => {
-			val ys = subs(t)
-			(ys ++ ys.view.map(h :: _))
-		}
-	}
+  def subs[T](l: List[T]) : List[List[T]] = l match {
+    case Nil => List(List())
+    case h :: t => {
+      val ys = subs(t)
+      (ys ++ ys.view.map(h :: _))
+    }
+  }
 
-	def subbags[T](l: List[T]) : List[List[T]] =
-		for {	ys <- subs(l);
-					zs <- ys.permutations.toList
-		} yield zs
+  def subbags[T](l: List[T]) : List[List[T]] =
+    for { ys <- subs(l);
+          zs <- ys.permutations.toList
+    } yield zs
 
 
-	// Execution
-	val exec = IO {
-		println(solutions(List(1, 3, 7, 10, 25, 50), 765).mkString("\n"))
-	}
+  // Execution
+  val exec = IO {
+    println(solutions(List(1, 3, 7, 10, 25, 50), 765).mkString("\n"))
+  }
 
-	// Change the world and calculate!
-	exec.unsafePerformIO
-
+  exec.unsafePerformIO
 }
--- src/main/scala/WordGame.scala
+++ src/main/scala/WordGame.scala
@@ -4,24 +4,25 @@ import io.Source
 import scalaz._, Scalaz._, effect._
 
 object WordGame extends App {
-	
-	// Create the unsafe IO operation that will load the words
-	val loadDictionaryAction = IO { 
-		Source.fromFile("/Users/James/Desktop/cntdn/src/main/resources/dictionary.txt").getLines.toStream 
-	}
+  
+  // Create the unsafe IO operation that will load the words
+  val loadDictionaryAction = IO { 
+    Source.fromFile("/Users/James/Desktop/cntdn/src/main/resources/dictionary.txt").getLines.toStream 
+  }
 
-	// Change the world by loading the dictionary!
-	val words = loadDictionaryAction.unsafePerformIO.toList
+  // Change the world by loading the dictionary!
+  val words = loadDictionaryAction.unsafePerformIO.toList
 
-	// Characters we can use - first half of the alphabet for a test!
-	val testString = ('a' |-> 'm') mkString
+  // Characters we can use - first half of the alphabet for a test!
+  val testString = ('a' |-> 'm') mkString
 
-	// Can a word s be made using the testString characters tstr?
-	def canBeMade(s: List[Char], tstr: String = testString) : Boolean = s match {
-		case Nil 		=> true
-		case h :: t => tstr.contains(h) ? canBeMade(t, tstr diff h.point[List]) | false
-	}
-	
-	// Make a list of words than can be made with these characters
-	val canMake = words filter { word => canBeMade(word.toList) } println
+  // Can a word s be made using the testString characters tstr?
+  def canBeMade(s: List[Char], tstr: String = testString) : Boolean = s match {
+    case Nil    => true
+    case h :: t => tstr.contains(h) ? canBeMade(t, tstr diff h.point[List]) | false
+  }
+  
+  // Make a list of words than can be made with these characters
+  val canMake = words filter { word => canBeMade(word.toList) } println
+  
 }
